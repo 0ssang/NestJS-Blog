4eaa591afdf4ed1d22ee4d42c91af9c14a3b789a
조영상 <51320595+0ssang@users.noreply.github.com>
2024-09-27 00:55:52 +0900

    feat: 영속성(Repository) 계층 파일 작성
    Repository 인터페이스 및 구현 클래스 작성

---
+++ src/blog.repository.ts
@@ -0,0 +1,59 @@
+import { readFile, writeFile } from 'fs/promises';
+import { PostDto } from './blog.model';
+
+// blog Repository Interface
+export interface BlogRepository {
+    getAllPost(): Promise<PostDto[]>;
+    createPost(postDto: PostDto);
+    getPost(id: string): Promise<PostDto>;
+    deletePost(id: string);
+    updatePost(id: string, postDto: PostDto);
+}
+
+// 구현 Class: 파일 읽고 쓰기
+export class BlogFileRepository implements BlogRepository {
+    FILE_NAME = './src/blog.data.json';
+
+    // 파일을 읽어서 모든 게시글 불러오기
+    async getAllPost(): Promise<PostDto[]> {
+        const datas = await readFile(this.FILE_NAME, 'utf-8');
+        const posts = JSON.parse(datas);
+        return posts;
+    }
+
+    // 게시글 쓰기
+    async createPost(postDto: PostDto) {
+        const posts = await this.getAllPost();
+        const id = posts.length + 1;
+        const createPost = {
+            id: id.toString(),
+            ...postDto,
+            createdDt: new Date(),
+        };
+        posts.push(createPost);
+        await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    }
+
+    // 게시글 하나 가져오기
+    async getPost(id: string): Promise<PostDto> {
+        const posts = await this.getAllPost();
+        const result = posts.find((post) => post.id === id);
+        return result;
+    }
+
+    // 게시글 하나 삭제
+    async deletePost(id: string) {
+        const posts = await this.getAllPost();
+        const filteredPosts = posts.filter((post) => post.id !== id);
+        await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts));
+    }
+
+    // 게시글 하나 수정하기
+    async updatePost(id: string, postDto: PostDto) {
+        const posts = await this.getAllPost();
+        const index = posts.findIndex((post) => post.id === id);
+        const updatePost = { id, ...postDto, updatedDt: new Date() };
+        posts[index] = updatePost;
+        await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    }
+}
