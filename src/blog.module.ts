fd066681907268c306bab5efe8e8e5404cca80f3
조영상 <51320595+0ssang@users.noreply.github.com>
2024-09-30 23:58:41 +0900

    refactor: MongoDB , Provider 설정 변경

--- src/blog.module.ts
+++ src/blog.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
-import { BlogFileRepository } from './blog.repository';
+import { BlogFileRepository, BlogMongoRepository } from './blog.repository';
+import { Blog, BlogSchema } from './blog.schema';
 
 @Module({
-    imports: [],
+    imports: [
+        // MongoDB 연결 설정
+        MongooseModule.forRoot(
+            'mongodb+srv://detective:ysh31228*@cluster0.4huxy.mongodb.net/blog',
+        ),
+        // 몽고디비 스키마 설정
+        MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    ],
     controllers: [BlogController],
-    providers: [BlogService, BlogFileRepository],
+    providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
