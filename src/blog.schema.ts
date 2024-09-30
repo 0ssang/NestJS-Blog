d8c9206b9ca9560716f416b022cbcf4b9f86e5ee
조영상 <51320595+0ssang@users.noreply.github.com>
2024-09-30 23:20:10 +0900

    feat: Mongoose 스키마 작성
    Blog 타입이면서 MongoDB의 Document타입으로 사용할 수 있는 BlogDocument 타입 생성
    @Schema 데코레이터를 사용하여 모델 정의

---
+++ src/blog.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// 블로그이면서 도큐먼틘 타입 정의
+export type BlogDocument = Blog & Document;
+
+@Schema()
+export class Blog {
+    @Prop()
+    id: string;
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    content: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    createDt: Date;
+
+    @Prop()
+    updateDt: Date;
+}
+
+// 스키마를 생성함 내부적으로는 mongoose의 new Schema를 사용한다.
+export const BlogSchema = SchemaFactory.createForClass(Blog);
